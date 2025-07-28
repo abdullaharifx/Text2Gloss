3d131c984a98cca27dbd9da8448b99fe5a4d6e06
Abdullah Arif <80403594+abdullaharifx@users.noreply.github.com>
2025-07-28 08:33:56 +0500

    Update mbart_model.py

--- models/mbart_model.py
+++ models/mbart_model.py
@@ -1,6 +1,6 @@
 from transformers import MBartForConditionalGeneration, MBartTokenizer
 
-def load_model_and_tokenizer_cc25(model_name="facebook/mbart-large-cc25", lang="en_XX"):
+def load_model_and_tokenizer(model_name="facebook/mbart-large-cc25", lang="en_XX"):
     tokenizer = MBartTokenizer.from_pretrained(model_name, src_lang=lang, tgt_lang=lang)
     model = MBartForConditionalGeneration.from_pretrained(model_name)
     model.config.decoder_start_token_id = tokenizer.lang_code_to_id[lang]
